e5d10d5667831506763c61f99686d1ea4ef51ed4
Manan Desai <manan1998@gmail.com>
2019-10-10 13:11:45 -0400

    adding plotly and interactive graph

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 
 Creating a plot in R of the average franchise values from 2002-2018 for the four major sports leagues in America.
 
-Made to explore using ggplot2 in R.
+Made to explore using ggplot2 and plotly in R.
 
-The resulting plot follows.
-
-![Plot](plot.png)
+The resulting plot can be found at [Plot](plot.png). The interactive plot using plotly can be found at [Interactive Plot](graph.html).
 
 ##### Sources
 [NBA](https://www.statista.com/statistics/193442/average-franchise-value-in-the-nba-since-2000/)
--- valueplot.R
+++ valueplot.R
@@ -1,7 +1,9 @@
 #plot 4 major sports leagues average franchise values
 install.packages("ggplot2")
+install.packages("plotly")
 library(ggplot2)
 library(reshape2)
+library(plotly)
 
 MLB <- read.csv("mlb-avg-value.csv", header = FALSE)
 NBA <- read.csv("nba-avg-value.csv", header = FALSE)
@@ -25,9 +27,12 @@ fulldata$NHL <- gsub(",", "", fulldata$NHL)
 
 melted <- melt(fulldata, id.vars = "Year")
 melted$value <- as.numeric(melted$value)
-ggplot(data = melted, aes(x = Year, y = value, group = factor(variable))) + 
+p <- ggplot(data = melted, aes(x = Year, y = value, group = factor(variable))) + 
   geom_line(size=0.5, aes(color=variable), alpha=1) +
   labs(title = "Average Franchise Value of 4 Major Sports Leagues from 2002 to 2018", 
        y = "Franchise Value (in Millions USD)", 
        x = "Year") +
   theme(plot.title = element_text(size = 8, hjust = 0.5))
+
+pl <- ggplotly(p)
+htmlwidgets::saveWidget(as_widget(pl), "graph.html")
