ff14e9def44f762d5d5c915c901d63e37e4d4827
Manan Desai <manan1998@gmail.com>
2019-10-10 12:03:53 -0400

    adding source files, R file, and picture of plot

---
+++ README.md
@@ -0,0 +1,8 @@
+
+![Plot](plot.png)
+
+##### Sources
+[NBA](https://www.statista.com/statistics/193442/average-franchise-value-in-the-nba-since-2000/)
+[NFL](https://www.statista.com/statistics/193435/average-franchise-value-in-the-nfl-since-2000/)
+[NHL](https://www.statista.com/statistics/193443/average-franchise-value-nhl/)
+[MLB](https://www.statista.com/statistics/193441/average-franchise-value-in-mlb-since-2000/)
---
+++ valueplot.R
@@ -0,0 +1,33 @@
+#plot 4 major sports leagues average franchise values
+install.packages("ggplot2")
+library(ggplot2)
+library(reshape2)
+
+MLB <- read.csv("mlb-avg-value.csv", header = FALSE)
+NBA <- read.csv("nba-avg-value.csv", header = FALSE)
+NFL <- read.csv("nfl-avg-value.csv", header = FALSE)
+NHL <- read.csv("nhl-avg-value.csv", header = FALSE)
+
+names(MLB) <- c("Year", "Average MLB Franchise Value")
+names(NBA) <- c("Year", "Average NBA Franchise Value")
+names(NFL) <- c("Year", "Average NFL Franchise Value")
+names(NHL) <- c("Year", "Average NHL Franchise Value")
+
+fulldata <- merge(MLB, NBA)
+fulldata <- merge(fulldata, NFL)
+fulldata <- merge(fulldata, NHL)
+names(fulldata) <- c("Year", "MLB", "NBA", "NFL", "NHL")
+#remove commas
+fulldata$MLB <- gsub(",", "", fulldata$MLB)
+fulldata$NBA <- gsub(",", "", fulldata$NBA)
+fulldata$NFL <- gsub(",", "", fulldata$NFL)
+fulldata$NHL <- gsub(",", "", fulldata$NHL)
+
+melted <- melt(fulldata, id.vars = "Year")
+melted$value <- as.numeric(melted$value)
+ggplot(data = melted, aes(x = Year, y = value, group = factor(variable))) + 
+  geom_line(size=0.5, aes(color=variable), alpha=1) +
+  labs(title = "Average Franchise Value of 4 Major Sports Leagues from 2002 to 2018", 
+       y = "Franchise Value (in Millions USD)", 
+       x = "Year") +
+  theme(plot.title = element_text(size = 8, hjust = 0.5))
